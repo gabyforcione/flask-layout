ac1f4bfb8b6e6ea481dc96d5b7b096cb2c3954df
gabyforcione <gabriellyforcione@colegiopaulodetarso.com.br>
2026-06-23 14:46:53 +0000

    oi

--- app.py
+++ app.py
@@ -4,8 +4,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    aluno = {
+        "nome": "gaby forcione",
+        "turma": "1 medio"
+    }
+    professor = [
+        {
+            "nome": "Felipe Ishara",
+            "materia": "web"
+        },
+        {
+            "nome": "welligton",
+            "materia": "historia"      
+        }
+    ]
+    return render_template('index.html', title='home', aluno=aluno)
 
 @app.route("/boletim")
 def boletim():
-    return render_template('boletim.html')
+    return render_template('boletim.html', title='boletim')
